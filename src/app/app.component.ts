c1536230a9a2c65a4fd6a8295d4ffc9aa4f168ab
phuchh27 <phuchh27@gmail.com>
2023-06-24 15:33:16 +0700

    simple reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,14 +19,30 @@ export class AppComponent implements OnInit {
           Validators.required,
           this.forbiddenNames.bind(this),
         ]),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        email: new FormControl(null, [Validators.required, Validators.email ], this.forbiddenEmails),
       }),
       gender: new FormControl('male'),
       hobbies: new FormArray([]),
     });
+    // this.signUpForm.valueChanges.subscribe((value) => {
+    //   console.log(value);
+    // })
+    this.signUpForm.statusChanges.subscribe((status) => {
+        console.log(status);
+      })
+    this.signUpForm.setValue({
+      'userData': {'username': 'Max', 'email': 'mmax@test.com'},
+      'gender':'male',
+      'hobbies': [],
+    })
+    this.signUpForm.patchValue({
+      'userData': {'username': 'Anna', },
+
+    })
   }
   onSubmit() {
     console.log(this.signUpForm);
+    this.signUpForm.reset();
   }
 
   onAddHobby() {
@@ -41,5 +58,18 @@ export class AppComponent implements OnInit {
       return { nameIsForbidden: true };
     }
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject)=>{
+      setTimeout(()=>{
+        if (control.value === 'test@test.com') {
+          resolve({emailIsForbidden: true});
+        } else {
+          resolve(null);
+        }
+      },1500);
+    });
+    return promise;
   }
 }
